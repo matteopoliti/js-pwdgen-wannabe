5716e77c83d016f6552f22aa5acdd87d6f4141a3
matteopoliti <matteo.politi17@gmail.com>
2023-12-21 15:17:29 +0100

    feat: add js function

---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+let nome = prompt("Nome")
+console.log(nome)
+
+let cognome = prompt("Cognome")
+console.log(cognome)
+
+let color = prompt("Colore preferito")
+console.log(color)
+
+document.writeln(`${nome+cognome+color}23`)
